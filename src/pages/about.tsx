4081c40568a8c7e2115c80b56e1a942788b2952e
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2022-10-06 17:26:02 +0200

    add some components to test suspense

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router-dom";
-
+import React, { Suspense } from "react";
+import Button from "../components/Button";
+const LazyButton = React.lazy(() => {
+  return Promise.all([
+    import("../components/Button"),
+    new Promise((resolve) => setTimeout(resolve, 3000)),
+  ]).then(([moduleExports]) => moduleExports);
+});
 function About() {
   return (
     <>
       <div>About</div>
       <Link to="/">Go to Home</Link>
+      <Button label={"Normal Button"} />
+      <Suspense fallback={<div>Loading button...</div>}>
+        <LazyButton label={"Lazy Button (3000ms)"} />
+      </Suspense>
     </>
   );
 }
